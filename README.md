f9a05b20db47aedb27f6bd598313f2495aaca015
bracinho2 <bracinho2@hotmail.com>
2022-07-14 07:13:55 -0300

    atualização do readme.

--- README.md
+++ README.md
@@ -1,16 +1,28 @@
-# qrg
+# QRG
 
-A new Flutter project.
+Uma ferramenta para os Radioamadores!
 
-## Getting Started
+## Descrição
 
-This project is a starting point for a Flutter application.
+Alô Google!
 
-A few resources to get you started if this is your first Flutter project:
+Este Aplicativo é uma tentativa de construção de um App com Flutter!
+Qual é o objetivo?
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+- Trazer uma lista atualizada das nossas Repetidoras;
+- Trazer uma lista atualizada dos nossos IVGs;
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+O que isto significa?
+
+No Brasil uma pessoa pode se tornar radioamador (a) com 10 anos!
+É um hobby altamente recomendável para todas as idades!
+
+Para que serve as repetidoras?
+
+- Para fortalecer e conectar os amigos radioamadores!
+
+Para que serve um IVG?
+
+- Para conectar os amigos mais distantes!
+
+## Forte 73
--- lib/app/core/widgets/custom_visible_widget.dart
+++ lib/app/core/widgets/custom_visible_widget.dart
@@ -1,21 +1,37 @@
 import 'package:flutter/material.dart';
 
 class VisibilidadePersonalizada extends StatelessWidget {
-  //este widget receberá via construtor;
-  final Widget child;
-  //o valor padrão será enviado via construtor;
-  final bool active;
+  final String chave;
+  final Widget filho;
+  final bool valorPadrao;
   const VisibilidadePersonalizada({
     Key? key,
-    required this.child,
-    required this.active,
+    required this.chave,
+    required this.filho,
+    required this.valorPadrao,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Visibility(
-      visible: active,
-      child: child,
+      visible: MinhaClasseQueMostraWidgets()
+          .recuperaUmaInformacao(chave: chave, valorPadrao: valorPadrao),
+      child: filho,
     );
+  }
+}
+
+class MinhaClasseQueMostraWidgets {
+  bool recuperaUmaInformacao({
+    required String chave,
+    required bool valorPadrao,
+  }) {
+    // Aqui procuramos em algum lugar: cache, sharedPreferences, Firebase...
+    //  _resultado = ProcuroEmAlgumLugar(chave);
+    //  Verifico o Resultado:
+    //  return _resultado != valorPadrao ? _resultado : valorPadrao;
+    // Exemplo:
+    var _resultado = true; //valor ficticio;
+    return _resultado != valorPadrao ? _resultado : valorPadrao;
   }
 }
